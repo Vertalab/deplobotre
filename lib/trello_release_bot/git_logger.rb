2553c69f242855f0fbe0b0d05601c11b1d968a0d
Roman Vertalab <roman@vertalab.com>
2016-07-14 11:23:39 +0300

    add end of commit splitter

--- lib/trello_release_bot/git_logger.rb
+++ lib/trello_release_bot/git_logger.rb
@@ -1,9 +1,11 @@
 module TrelloReleaseBot
   module GitLogger
+    EOC = "\n\n|EOC|\n\n".freeze
+
     # @param [String] branch name
     # @param [Time] since time
     def self.commits(repo_path, revission_rage)
-      raw_logs = `cd #{repo_path} && git log #{revission_rage} --format="%H %B"`
+      raw_logs = `cd #{repo_path} && git log #{revission_rage} --format="%H %B#{EOC}"`
       raw_logs.split("\n\n").map do |raw_log|
         { id: raw_log[/^[^ ]*/], message: raw_log[/(?<=\s).*/] }
       end
