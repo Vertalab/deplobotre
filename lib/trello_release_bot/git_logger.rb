b3fcbf499a5bad8dbb7062b50f3c6daecd72071f
Roman Vertalab <roman@vertalab.com>
2016-07-15 11:57:08 +0300

    fix commits parsing

--- lib/trello_release_bot/git_logger.rb
+++ lib/trello_release_bot/git_logger.rb
@@ -6,7 +6,7 @@ module TrelloReleaseBot
     # @param [Time] since time
     def self.commits(repo_path, revission_rage)
       raw_logs = `cd #{repo_path} && git log #{revission_rage} --format="%H %B#{EOC}"`
-      raw_logs.split("\n\n").map do |raw_log|
+      raw_logs.split(EOC + "\n").map do |raw_log|
         { id: raw_log[/^[^ ]*/], message: raw_log[/(?<=\s).*/] }
       end
     end
