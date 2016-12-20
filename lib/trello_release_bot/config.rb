9ae67ac3d6d6be22210a29b0604f95125829e4dc
David Almeida <david@skore.io>
2016-12-20 21:02:26 -0200

    teste 2

--- lib/trello_release_bot/config.rb
+++ lib/trello_release_bot/config.rb
@@ -1,9 +1,8 @@
 module TrelloReleaseBot
   class Config
-    attr_accessor :trello_token, :trello_key, :board_id, :commits_url
+    attr_accessor :trello_token, :trello_key, :board_id
 
     def initialize
-      @commits_url = nil
       @trello_token = fetch(:trello_token)
       @trello_key = fetch(:trello_key)
       @board_id = fetch(:board_id)
